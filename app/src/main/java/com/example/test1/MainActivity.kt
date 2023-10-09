f5130026cabfd1fa86c57746d2283d4e61b15705
Daniyar234 <belgibekovdaniar@gmail.com>
2023-10-10 00:13:05 +0600

    HW 2

---
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.test1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.test1.ui.theme.Test1Theme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Test1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    GreetingText(message = "Hello, happy Birthday Sam", from = " from Daniyar")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+
+    Column(modifier = modifier.padding(8.dp), verticalArrangement = Arrangement.Center) {
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 96.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = modifier.padding(19.dp).align(alignment = Alignment.End)
+            
+        )
+    }
+    }
+
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    Test1Theme {
+        GreetingText(message = "Hello, happy Birthday Sam", from = " from Daniyar")
+    }
+}
